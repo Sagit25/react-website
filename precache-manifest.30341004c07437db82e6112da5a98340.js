775bbbb0326426467052957857ed2f4778b0b462
Sagit25 <sukhuny@snu.ac.kr>
2023-02-09 19:55:18 +0900

    Updates

---
+++ precache-manifest.30341004c07437db82e6112da5a98340.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bca48b47cc207f13df2",
+    "url": "/react-sebsite/static/css/main.27af247b.chunk.css"
+  },
+  {
+    "revision": "1bca48b47cc207f13df2",
+    "url": "/react-sebsite/static/js/main.1bca48b4.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/react-sebsite/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/react-sebsite/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "4cc72bde61c82be5c816",
+    "url": "/react-sebsite/static/js/runtime~main.4cc72bde.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-sebsite/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "f6748737309653ac8d1257fb511fda3a",
+    "url": "/react-sebsite/index.html"
+  }
+];
