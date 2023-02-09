66940ed8363c31ae027a4c9348157765da0598e6
Sagit25 <sukhuny@snu.ac.kr>
2023-02-10 00:57:23 +0900

    basic skeleton

---
+++ src/components/CV.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./CV.css"
+
+function CV() {
+  return (
+    <section>
+      <h2 className="small-title"> Education </h2>
+      <h1> Seoul National University (SNU) </h1>
+      <h2 className="small-title"> Experience </h2>
+      <h2 className="small-title"> Teaching </h2>
+    </section>
+  );
+}
+  
+export default CV;
